3d0e3402a6d5da917373858cfd53e9358ed79960
sophiayjcho <sophiayejeongcho@gmail.com>
2022-06-10 14:22:13 -0400

    dealing with developer api

--- commands/tft_search.js
+++ commands/tft_search.js
@@ -2,36 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
 const log4js = require('log4js');
 const logger = log4js.getLogger('log');
-const { devAPI, leagueVersion } = require('../config.json');
-const { Kayn, REGIONS, KaynRequest } = require('kayn');
+const {devAPI, leagueVersion } = require('../config.json');
 const TeemoJS = require('teemojs');
-
 let api = TeemoJS(devAPI);
-const kayn = Kayn(devAPI)({
-    region: REGIONS.NORTH_AMERICA,
-    apiURLPrefix: 'https://%s.api.riotgames.com',
-    locale: 'en_US',
-    debugOptions: {
-        isEnabled: true,
-        showKey: false,
-    },
-    requestOptions: {
-        shouldRetry: true,
-        numberOfRetriesBeforeAbort: 3,
-        delayBeforeRetry: 1000,
-        burst: false,
-        shouldExitOn403: false,
-    },
-    cacheOptions: {
-        cache: null,
-        timeToLives: {
-            useDefault: false,
-            byGroup: {},
-            byMethod: {},
-        },
-    },
-});
-
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -51,13 +24,16 @@ module.exports = {
 
         async function embed()
         {
-
-            const summonerv4 = await kayn.Summoner.by.name(name);
-            let iconURL = 'http://ddragon.leagueoflegends.com/cdn/' + leagueVersion + '/img/profileicon/' + summonerv4.profileIconId + '.png';
-            let lolchessURL = 'https://lolchess.gg/profile/na/' + summonerv4.name.replace(/ /g,'');
+            let iconURL, lolchessURL, summonerv4;
+            await api.get('na1', 'summoner.getBySummonerName', name)
+                .then(data => { console.log(data); iconURL = 'http://ddragon.leagueoflegends.com/cdn/' + leagueVersion + '/img/profileicon/' + data.profileIconId + '.png';
+                lolchessURL = 'https://lolchess.gg/profile/na/' + data.name.replace(/ /g,'');
+            summonerv4 = data;});
+           
             let tft, embeds;
             await api.get('na1', 'tftLeague.getLeagueEntriesForSummoner', summonerv4.id)
                 .then(data => {
+                                console.log(data);
                                 tft = data;
                                 let color = 0xa195d9;
                                 let winrate = (tft[0].wins / (tft[0].wins + tft[0].losses) * 100).toFixed(2);
